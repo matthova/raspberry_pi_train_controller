ce69cfe409d61e4a4591c33c3ab5a475319b64d9
matthova <matthova87@gmail.com>
2014-12-10 09:18:48 +0000

    post led control working

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+from RPIO import PWM
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def form():
+	PWM.add_channel_pulse(0, 17, 0, int(request.form["hello"]))
+	print request.form["hello"]
+	return "Hello"
--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 #!flask/bin/python
 from app import app
-app.run(debug=True)
+from RPIO import PWM
+
+PWM.setup()
+PWM.init_channel(0)
+try:
+	app.run(debug=True, host='0.0.0.0')
+except Exception:
+	PWM.cleanup()
