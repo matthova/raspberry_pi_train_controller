bee7f677354101da070d0de9bc7a2b6079f02725
matthova <matthova87@gmail.com>
2014-12-11 23:58:42 +0000

    added direction control

--- app/views.py
+++ app/views.py
@@ -1,9 +1,20 @@
 from flask import render_template, request
 from app import app
+
+forward_pin = 7
+reverse_pin = 8
+speed_pin = 17
 try:
+	import RPIO
 	from RPIO import PWM
+	
+	RPIO.setup(forward_pin, RPIO.OUT)
+	RPIO.setup(reverse_pin, RPIO.OUT)
+	RPIO.output(forward_pin, False)
+	RPIO.output(reverse_pin, False)
+
 except Exception:
-	print 'No RPIO available'
+	print 'No RPIO available mmk'
 
 @app.route('/', methods=['GET'])
 def index():
@@ -12,7 +23,14 @@ def index():
 @app.route('/', methods=['POST'])
 def form():
 	try:
-		PWM.add_channel_pulse(0, 17, 0, int(request.form["hello"]))
+		PWM.add_channel_pulse(0, speed_pin, 0, int(request.form["hello"]))
+		print request.form["direction"]
+		if int(request.form["direction"]) == 1:
+			RPIO.output(forward_pin, True)
+			RPIO.output(reverse_pin, False)
+		else:
+			RPIO.output(forward_pin, False)
+			RPIO.output(reverse_pin, True)
 	except Exception:
 		print 'No RPIO available'
 
