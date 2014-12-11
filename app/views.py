7f33444755bac7c2a3b93b427fb35ac21f094bff
Matt Hova <matt@igroupcreative.com>
2014-12-11 12:09:55 -0800

    wip

--- app/views.py
+++ app/views.py
@@ -1,17 +1,25 @@
 from flask import render_template, request
 from app import app
-from app import crossdomain
 try:
 	from RPIO import PWM
 except Exception:
 	print 'No RPIO available'
+
+def add_cors_headers(response):
+	response.headers['Access-Control-Allow-Origin'] = '*'
+	if request.method == 'OPTIONS':
+		response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+		headers = request.headers.get('Access-Control-Request-Headers')
+		if headers:
+			response.headers['Access-Control-Allow-Headers'] = headers
+	return response
+app.after_request(add_cors_headers)
 
 @app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-@crossdomain(origin='*')
 def form():
 	try:
 		PWM.add_channel_pulse(0, 17, 0, int(request.form["hello"]))
@@ -20,3 +28,4 @@ def form():
 
 	print request.form["hello"]
 	return "Hello"
+
--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@ sudo apt-get install python-virtualenv
 virtualenv flask
 flask/bin/pip install flask
 flask/bin/pip install RPIO
-flask/bin/pip install flask-cors
